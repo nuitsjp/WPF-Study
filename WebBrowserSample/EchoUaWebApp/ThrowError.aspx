99cfae7d60d28d6669da115b978fa452d06b93ed
nuits.jp <nuits.jp@live.jp>
2016-06-25 19:55:28 +0900

    SuppressJavaScriptErrorBrowserの追加

---
+++ WebBrowserSample/EchoUaWebApp/ThrowError.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ThrowError.aspx.cs" Inherits="EchoUaWebApp.ThrowError" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<script lang="JavaScript">
+<!--
+function throwError(){
+    throw new Error("エラー発生");
+}
+// -->
+</script>
+<body onload="throwError()">
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebBrowserSample/SuppressJavaScriptErrorBrowser/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SuppressJavaScriptErrorBrowser
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // JavaScriptのエラー表示を抑止する
+            var axIWebBrowser2 = typeof(WebBrowser).GetProperty("AxIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            var comObj = axIWebBrowser2.GetValue(webBrowser, null);
+            comObj.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, comObj, new object[] { true });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Navigate(addressTextBox.Text);
+        }
+
+    }
+}
