7198ce1070adaa0a237b0d5a20d61a2ab3cc9321
nuits.jp <nuits.jp@live.jp>
2016-06-25 14:00:11 +0900

    UaImpersonationBrowser追加

---
+++ WebBrowserSample/EchoUaWebApp/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EchoUaWebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="UserAgent："></asp:Label>
+        <asp:Label ID="UserAgentLabel" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" Text="もう一回" />
+    </form>
+</body>
+</html>
---
+++ WebBrowserSample/UaImpersonationBrowser/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UaImpersonationBrowser
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("urlmon.dll", CharSet = CharSet.Ansi)]
+        private static extern int UrlMkSetSessionOption(int dwOption, string pBuffer, int dwBufferLength, int dwReserved);
+        private const int URLMON_OPTION_USERAGENT = 0x10000001;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var userAgent = "hoge";
+            UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, userAgent, userAgent.Length, 0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Navigate(addressTextBox.Text);
+        }
+    }
+}
