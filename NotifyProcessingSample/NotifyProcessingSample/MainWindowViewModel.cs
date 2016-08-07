ce82203d7fe5fe51b674cb5276e579c12545422c
nuits.jp <nuits.jp@live.jp>
2016-08-07 20:17:09 +0900

    NotifyProcessingSampleプロジェクト追加

---
+++ NotifyProcessingSample/NotifyProcessingSample/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace NotifyProcessingSample
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private bool _processing;
+        public bool Processing
+        {
+            get { return _processing; }
+            set { Set(ref _processing, value); }
+        }
+        public ICommand HeavyCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            HeavyCommand = new RelayCommand(async () =>
+            {
+                Processing = true;
+                try
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                }
+                finally
+                {
+                    Processing = false;
+                }
+            });
+        }
+    }
+}
