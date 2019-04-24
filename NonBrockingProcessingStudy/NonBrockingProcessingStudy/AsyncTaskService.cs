b2a74f7d7f3abc69076ee1ec60c198a5624c304d
Atsushi Nakamura <nuits.jp@live.jp>
2019-04-25 01:51:41 +0900

    Add NonBrockingProcessingStudy.

---
+++ NonBrockingProcessingStudy/NonBrockingProcessingStudy/AsyncTaskService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonBrockingProcessingStudy
+{
+    public class AsyncTaskService
+    {
+        private IList<Task> Tasks { get; } = new SynchronizedCollection<Task>();
+
+        public Task Run(Action action)
+        {
+            var task = Task.Run(action);
+            task.GetAwaiter().OnCompleted(() => Tasks.Remove(task));
+            Tasks.Add(task);
+            return task;
+        }
+
+        public void WaitAll()
+        {
+            Task.WaitAll(Tasks.ToArray());
+        }
+    }
+}
---
+++ NonBrockingProcessingStudy/NonBrockingProcessingStudy/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PropertyChanged;
+using Reactive.Bindings;
+
+namespace NonBrockingProcessingStudy
+{
+    public class MainWindowViewModel
+    {
+        private AsyncTaskService AsyncTaskService { get; } = new AsyncTaskService();
+        public ReactiveProperty<bool> IsProcessing { get; } = new ReactiveProperty<bool>();
+
+        public ReactiveCommand HeavyCommand { get; } = new ReactiveCommand();
+
+        public ReactiveCommand ClosingCommand { get; } = new ReactiveCommand();
+
+        public MainWindowViewModel()
+        {
+            HeavyCommand.Subscribe(HeavyProcess);
+            ClosingCommand.Subscribe(() =>
+            {
+                AsyncTaskService.WaitAll();
+            });
+        }
+
+        private void HeavyProcess()
+        {
+            IsProcessing.Value = true;
+            AsyncTaskService.Run(() =>
+            {
+                Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+                IsProcessing.Value = false;
+            });
+        }
+    }
+}
